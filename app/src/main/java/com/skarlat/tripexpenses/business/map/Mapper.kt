cd421431e1bcd46ba1497b2998ee5a25caf865ef
Denis Skarlat <bboyzlodey@gmail.com>
2022-01-14 01:42:14 +0300

    Add expense map from entity to UI model

--- app/src/main/java/com/skarlat/tripexpenses/business/map/Mapper.kt
+++ app/src/main/java/com/skarlat/tripexpenses/business/map/Mapper.kt
@@ -1,12 +1,28 @@
 package com.skarlat.tripexpenses.business.map
 
-import com.skarlat.tripexpenses.data.local.entity.Trip
+import com.skarlat.tripexpenses.data.local.entity.Expense as ExpenseEntity
+import com.skarlat.tripexpenses.data.local.entity.Trip as TripEntity
+import com.skarlat.tripexpenses.ui.model.Expense as ExpenseUIModel
+import com.skarlat.tripexpenses.ui.model.Trip as TripUIModel
 
-fun Iterable<Trip>.mapToUIModel(): List<com.skarlat.tripexpenses.ui.model.Trip> {
-    return mapNotNull { trip: Trip ->
-        com.skarlat.tripexpenses.ui.model.Trip(
+fun Iterable<TripEntity>.mapToUIModel(): List<TripUIModel> {
+    return mapNotNull { trip: TripEntity ->
+        TripUIModel(
             id = trip.id,
             name = trip.name
+        )
+    }
+}
+
+@JvmName("mapToUIModelExpenseEntity")
+fun Iterable<ExpenseEntity>.mapToUIModel() : List<ExpenseUIModel> {
+    return mapNotNull { entity: ExpenseEntity ->
+        ExpenseUIModel(
+            id = entity.id,
+            date = entity.date,
+            isPayed = false,
+            amount = "${entity.amount} RUB",
+            debt = "0 RUB"
         )
     }
 }
