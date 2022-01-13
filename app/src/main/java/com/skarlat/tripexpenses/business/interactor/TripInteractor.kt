1c422b4cefea8c7227d2657d25ed8d55c954489b
Denis Skarlat <bboyzlodey@gmail.com>
2022-01-14 02:43:32 +0300

    Update trip interactor

--- app/src/main/java/com/skarlat/tripexpenses/business/interactor/TripInteractor.kt
+++ app/src/main/java/com/skarlat/tripexpenses/business/interactor/TripInteractor.kt
@@ -1,16 +1,24 @@
 package com.skarlat.tripexpenses.business.interactor
 
+import com.skarlat.tripexpenses.business.map.mapToEntity
 import com.skarlat.tripexpenses.business.map.mapToUIModel
 import com.skarlat.tripexpenses.data.local.entity.Trip
+import com.skarlat.tripexpenses.data.repository.IParticipantRepository
 import com.skarlat.tripexpenses.data.repository.ITripRepository
+import com.skarlat.tripexpenses.ui.model.Participant
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import java.util.*
 
-class TripInteractor(private val tripRepository: ITripRepository) {
+class TripInteractor(
+    private val tripRepository: ITripRepository,
+    private val participantRepository: IParticipantRepository
+) {
 
-    suspend fun createTrip(name: String) {
-        tripRepository.createTrip(generateTrip(name))
+    suspend fun createTrip(name: String, participants: List<Participant>) {
+        val newTrip = generateTrip(name)
+        tripRepository.createTrip(newTrip)
+        participantRepository.addParticipants(participants.mapToEntity(newTrip.id))
     }
 
     private suspend fun generateTrip(tripName: String): Trip = Trip(
