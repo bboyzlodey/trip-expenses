fd1f5b768b21ecb2a008ff0010664e1e3df6cdd5
Denis Skarlat <bboyzlodey@gmail.com>
2022-01-14 15:20:21 +0300

    Add debt calculator

---
+++ app/src/main/java/com/skarlat/tripexpenses/business/calculator/IDebtorCalculator.kt
@@ -0,0 +1,31 @@
+package com.skarlat.tripexpenses.business.calculator
+
+import com.skarlat.tripexpenses.ui.model.Distribution
+
+interface IDebtorCalculator {
+
+    /**
+     *  Return map by participant id with total debt for this participant
+     * */
+    fun calculateDebits(distributions: Iterable<Distribution>): Map<String, Int>
+}
+
+class DebtCalculator : IDebtorCalculator {
+
+    override fun calculateDebits(distributions: Iterable<Distribution>): Map<String, Int> {
+        val debtorMap = mutableMapOf<String, Int>()
+        distributions.forEach { distribution ->
+            if (distribution.participantIds.size == 0 || distribution.cost.value == 0)
+                return@forEach
+            val debtForEach = distribution.cost.value / distribution.participantIds.size
+            distribution.participantIds.forEach { participantId ->
+                debtorMap.merge(
+                    participantId,
+                    debtForEach
+                ) { oldValue, newValue -> oldValue + newValue }
+            }
+        }
+        return debtorMap
+    }
+
+}
