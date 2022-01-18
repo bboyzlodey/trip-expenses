261bdac6fe46e7afdc5b6c920f8c32cb8027c737
Denis Skarlat <bboyzlodey@gmail.com>
2022-01-19 00:04:28 +0300

    Add cost calculator

---
+++ app/src/main/java/com/skarlat/tripexpenses/business/calculator/ICostCalculator.kt
@@ -0,0 +1,26 @@
+package com.skarlat.tripexpenses.business.calculator
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+interface ICostCalculator {
+    val totalCost: Flow<Int>
+
+    suspend fun onCostChanged(costId: String, newCost: String)
+}
+
+class TotalCostCalculator : ICostCalculator {
+
+    override val totalCost: Flow<Int>
+        get() = totalCostMutableFlow
+
+    private val totalCostMutableFlow = MutableStateFlow(0)
+    private val costMap = mutableMapOf<String, Int>()
+
+    override suspend fun onCostChanged(costId: String, cost: String) {
+        val newCost = cost.toIntOrNull() ?: 0
+        val totalCost = this.totalCostMutableFlow.value
+        val oldCost = costMap.getOrDefault(costId, 0)
+        totalCostMutableFlow.tryEmit(totalCost - oldCost + newCost)
+    }
+}
