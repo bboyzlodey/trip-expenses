6277119a90dac74ae1ab5681d43cd83016eceb75
Denis Skarlat <bboyzlodey@gmail.com>
2022-01-20 20:57:58 +0300

    Add navigation graph

--- app/src/main/java/com/skarlat/tripexpenses/MainActivity.kt
+++ app/src/main/java/com/skarlat/tripexpenses/MainActivity.kt
@@ -6,9 +6,18 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.skarlat.tripexpenses.ui.screen.CreateExpenseScreen
+import com.skarlat.tripexpenses.ui.screen.PreviewCreateTripScreen
+import com.skarlat.tripexpenses.ui.screen.PreviewExpenseListScreen
+import com.skarlat.tripexpenses.ui.screen.PreviewTripList
 import com.skarlat.tripexpenses.ui.theme.TripExpensesTheme
 import com.skarlat.tripexpenses.ui.viewModel.MainViewModel
+import com.skarlat.tripexpenses.utils.Const
 import com.skarlat.tripexpenses.utils.DialogFactory
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -24,6 +33,24 @@ class MainActivity : AppCompatActivity() /*,ComponentActivity()*/ {
             TripExpensesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Const.SCREEN_LIST_TRIP
+                    ) {
+                        composable(route = Const.SCREEN_LIST_TRIP) { navBackStackEntry ->
+                            PreviewTripList()
+                        }
+                        composable(route = Const.SCREEN_CREATE_TRIP) { navBackStackEntry ->
+                            PreviewCreateTripScreen()
+                        }
+                        composable(route = Const.SCREEN_LIST_EXPENSES) { navBackStackEntry ->
+                            PreviewExpenseListScreen()
+                        }
+                        composable(route = Const.SCREEN_CREATE_EXPENSE) { navBackStackEntry ->
+                            CreateExpenseScreen(viewModel = hiltViewModel())
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/skarlat/tripexpenses/utils/Const.kt
+++ app/src/main/java/com/skarlat/tripexpenses/utils/Const.kt
@@ -3,4 +3,10 @@ package com.skarlat.tripexpenses.utils
 object Const {
     const val SELF_ID = "SELF_ID"
     const val ALL_ID = "ALL_ID"
+
+    const val SCREEN_CREATE_EXPENSE = "createExpense"
+    const val SCREEN_LIST_EXPENSES = "listExpenses"
+    const val SCREEN_LIST_TRIP = "listTrip"
+    const val SCREEN_CREATE_TRIP = "createTrip"
+
 }
