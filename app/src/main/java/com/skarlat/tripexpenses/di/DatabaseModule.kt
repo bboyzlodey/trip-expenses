7a0a0837ff6e4984c40cea83243ed44b6a0b7063
Denis Skarlat <bboyzlodey@gmail.com>
2022-04-10 22:28:26 +0300

    Create participant with self_id in database

--- app/src/main/java/com/skarlat/tripexpenses/di/DatabaseModule.kt
+++ app/src/main/java/com/skarlat/tripexpenses/di/DatabaseModule.kt
@@ -2,7 +2,10 @@ package com.skarlat.tripexpenses.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.skarlat.tripexpenses.data.local.database.*
+import com.skarlat.tripexpenses.utils.Const
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,7 +21,13 @@ class DatabaseModule {
     @Provides
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
-        return Room.databaseBuilder(context, AppDatabase::class.java, "expenses.db").build()
+        return Room.databaseBuilder(context, AppDatabase::class.java, "expenses.db")
+            .addCallback(object : RoomDatabase.Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    db.execSQL("INSERT OR IGNORE INTO participant VALUES('${Const.SELF_ID}', '${Const.SELF_ID}', '${Const.ALL_TRIPS}')")
+                }
+            })
+            .build()
     }
 
     @Provides
--- app/src/main/java/com/skarlat/tripexpenses/utils/Const.kt
+++ app/src/main/java/com/skarlat/tripexpenses/utils/Const.kt
@@ -3,6 +3,7 @@ package com.skarlat.tripexpenses.utils
 object Const {
     const val SELF_ID = "SELF_ID"
     const val ALL_ID = "ALL_ID"
+    const val ALL_TRIPS = "ALL_TRIPS"
 
     const val SCREEN_CREATE_EXPENSE = "createExpense/{tripId}"
     const val SCREEN_LIST_EXPENSES = "listExpenses/{tripId}"
